7a138e3f88cf954c2c7a4682e6958b875576342e
kaora0 <Ka0rin@yandex.ru>
2024-04-16 19:43:30 +0300

    public

---
+++ Lab_31/Controllers/TestikController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_31.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestikController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index(string str)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Add(int age, string Name)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete() 
+        {
+            return Ok();
+        }
+    }
+}
